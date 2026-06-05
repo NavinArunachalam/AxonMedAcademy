e981823bafdc14faec75187697c97ce0d6bfa43c
NavinArunachalam <navinarunachalam20@gmail.com>
2026-06-05 15:34:54 +0530

    npm run build

--- client/.vercel/output/functions/__server.func/_ssr/index.mjs
+++ client/.vercel/output/functions/__server.func/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-DpQYLQjf.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-bVPMeQo0.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
